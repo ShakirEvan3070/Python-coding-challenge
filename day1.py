4491df8d7921e4f8606e783e513aff3ce1d0c27d
unknown <shakir.evan.3070@Fec.edu.bd>
2025-12-18 11:18:10 +0600

    day 2 going on

---
+++ day1.py
@@ -0,0 +1,181 @@
+# print("Hello World 🙂")
+# print("*"*10)
+
+# x =2 
+# if x == 2:
+#     print('x is 2 ')
+
+# myint = 7
+# print(myint)
+# myfloat = 8.0
+# print(myfloat)
+
+# one =1 ;
+# two = 2
+# three = one + two 
+# print(three)
+
+# hello = "Hello"
+# world = " world"
+# helloworld = hello + world
+# print(helloworld)
+
+# Problem 1 
+
+# num = 95
+# if num >= 95:
+#     print('Recently I heard that you’ve achieved\n 95% marks in your exam.\n This is brilliant!\n I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.')
+
+
+# problem 2
+# num1 , num2 = map(int, input().split())
+# sum = num1 + num2
+# sub = num1 -num2
+# mul = num1 * num2
+# div = num1 / num2
+
+# print(f"{num1} + {num2} = {sum}")
+# print(f"{num1} - {num2} = {sub}")
+# print(f"{num1} * {num2} = {mul}")
+# print(f"{num1} / {num2} = {div}")
+
+# problem 3
+
+# num = int(input())
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# problem 4
+
+# num = int(input())
+# if num > 0:
+#         print("positive")
+# elif num <0:
+#     print("negative")
+# else:
+#     print("zero")
+
+# problem 5
+# num = int(input())
+# if num >=10000:
+#     print('Gucci Bag')
+#     if num > 20000:
+#         print('Gucci Belt')
+# elif num>=5000:
+#     print('Levis Bag')
+# else:
+#     print('Something')
+
+# for i in range(5):
+#     print("Hello World")
+
+# text = input("Enter your text:")
+# for i in range(len(text)):
+#     print(text)
+
+# N = int(input("Enter a number: "))
+# total = 0
+# for i in range(1, N + 1):
+#     total = total + i
+# print("Sum of 1 to ", N , "is",total)
+
+
+# n = int(input())
+# if n%2 !=0:
+#     print("Weird")
+# elif n%2 ==0 and n in range(2,5):
+#     print('Not Weird')
+# elif n%2 ==0 and n in range(6,20):
+#     print('Weird')
+# elif n%2 ==0 and n>20:
+#     print('Not Weird')
+
+# a= int(input())
+# b= int(input())
+# sum = a + b
+# sub = a - b
+# mul = a * b
+# print(sum)
+# print(sub)
+# print(mul)
+
+# a = int(input())
+# b= int(input())
+
+# c= int(a/b)
+# d = float(a/b)
+# print(c)
+# print(d)
+
+# n = int(input())
+
+# for i in range(n):
+#     print(i * i)
+
+year = int(input())
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("True")
+else:
+    print("False")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day2.py
@@ -0,0 +1,12 @@
+# thisset = set(("apple", "banana", "cherry"))
+# for x in thisset:
+#     print(x)
+# print("banana" not in thisset)
+# thisset.add("orange")
+# print(thisset)
+
+# thisset = {"apple", "banana", "cherry"}
+# trophical = {"mango", "pineapple", "papaya"}
+# thisset.update(trophical)
+# print(thisset)
+
